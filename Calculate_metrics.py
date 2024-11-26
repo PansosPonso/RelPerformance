2ba5452efd36ea7934fb7d71acbab53169668702
Pansos <pansos@gmail.com>
2024-11-26 22:57:30 +0200

    Update Calculate_metrics.py

--- Calculate_metrics.py
+++ Calculate_metrics.py
@@ -2,15 +2,20 @@ import numpy as np
 import pandas as pd
 import warnings
 from sklearn.metrics import roc_auc_score
+import argparse
 from sklearn.isotonic import IsotonicRegression
 from sklearn.linear_model import LinearRegression
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
 
+# Set random seed for reproducibility
 RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
+#################### FUNCTIONS ####################
+
+# Function that calculates pAUC
 def calc_pauc(y_test, y_hat, fpr=0.2):
     pAUC = 0
     for i in range(5):
@@ -18,7 +23,7 @@ def calc_pauc(y_test, y_hat, fpr=0.2):
 
     return pAUC / 5
 
-
+# Function that calculates bootsrapped CI for pAUC
 def bootstrapped_CI_auc(y_true, y_pred):
     n_bootstraps = 1000
     bootstrapped_scores = []
@@ -43,6 +48,7 @@ def bootstrapped_CI_auc(y_true, y_pred):
 
     return confidence_lower, confidence_upper
 
+# Function that calculates bootsrapped CI for ACC
 def bootstrapped_CI_acc(y_true, y_pred):
     n_bootstraps = 1000
     bootstrapped_scores = []
@@ -64,6 +70,7 @@ def bootstrapped_CI_acc(y_true, y_pred):
 
     return confidence_lower, confidence_upper
 
+# Function that calculates ECE
 def calc_ece(samples, true_labels, M=5):
     # uniform binning approach with M number of bins
     bin_boundaries = np.linspace(0, 1, M + 1)
@@ -94,7 +101,7 @@ def calc_ece(samples, true_labels, M=5):
             ece += np.abs(avg_confidence_in_bin - accuracy_in_bin) * prob_in_bin
     return ece
 
-
+# Function that calibrates forecasts using an expanding window approach
 def isotonic_calibration(in_sample_preds, in_sample_y_true, out_sample_preds):
     calibrated_probs = np.zeros_like(out_sample_preds)
 
@@ -118,16 +125,19 @@ def isotonic_calibration(in_sample_preds, in_sample_y_true, out_sample_preds):
 
     return calibrated_probs / calibrated_probs.sum(axis=1).reshape(-1,1)
 
-
+# The main function that caclulates all evaluation metrics
 def caclulate_metrics(file_name, back):
 
     m_names = ['KDE','GM','MND','GC','NF','DeepAR','GAN','VAE','Lag-Llama','NB','SR','LGBM','RF','SVM','MLP','BVAR','PatchTST','EWMA']
 
+    # Get forecast data from all models
     forecasts_data = pd.read_excel(file_name, sheet_name='forecasts')
+    m_names = sorted([name for name in list(forecasts_data.columns) if "Unnamed" not in name][1:-1], key=lambda x: m_names.index(x))
+
     tickers = forecasts_data.iloc[2:102,1].values.tolist()
     forecasts = pd.DataFrame(
                 index=pd.MultiIndex.from_tuples([(d, asset) for d in forecasts_data.iloc[2:,0][~pd.isna(forecasts_data.iloc[2:,0])].values for asset in tickers], names=['Date', 'Asset']),
-                columns=pd.MultiIndex.from_tuples([(m, r) for m in [name for name in forecasts_data.columns if "Unnamed" not in name][1:len(m_names)+1]+['truth'] for r in ["Rank1", "Rank2","Rank3", "Rank4", "Rank5"]], names=['Models', 'Ranks']),
+                columns=pd.MultiIndex.from_tuples([(m, r) for m in [name for name in forecasts_data.columns if "Unnamed" not in name][1:] for r in ["Rank1", "Rank2","Rank3", "Rank4", "Rank5"]], names=['Models', 'Ranks']),
                 dtype=float
         )
 
@@ -158,37 +168,45 @@ def caclulate_metrics(file_name, back):
                 dtype=float
             )
 
-
+    # Calibrate forecasts using all observations until (but not including) date d and forecast for date d. We start from the second date, and therefore, for the first date, calibrated forecasts == forecasts
     calibrated_forecasts = forecasts.copy()
-    for c in m_names:
+    for c in m_names: # For each model
         print('Calibrate ', c)
-        for d in forecasts.index.get_level_values(0).unique()[1:]:
-            temp = forecasts.loc[forecasts.loc[:, (c, )].index.get_level_values(0) < d, (c, )].iloc[-back * 100:,:].reset_index().drop(columns=['Asset','Date'])
+        for d in forecasts.index.get_level_values(0).unique()[1:]: # For each month in the sample
+            X_train = forecasts.loc[forecasts.loc[:, (c, )].index.get_level_values(0) < d, (c, )].iloc[-back * 100:,:].reset_index().drop(columns=['Asset','Date'])
             y_train = forecasts.loc[forecasts.loc[:, ('truth', )].index.get_level_values(0) < d, ('truth', )].iloc[-back * 100:].values.argmax(axis=1)
-            calibrated_forecasts.loc[d, (c, )] = isotonic_calibration(temp.values, y_train, forecasts.loc[d, (c, )].values)
+            calibrated_forecasts.loc[d, (c, )] = isotonic_calibration(X_train.values, y_train, forecasts.loc[d, (c, )].values)
 
 
-    for c in m_names:
+    for c in m_names: # For each model
         print(c)
-        for i,d in enumerate(forecasts.index.get_level_values(0).unique()):
+        for i,d in enumerate(forecasts.index.get_level_values(0).unique()): # For each month in the sample
 
+            # Caclulate of forecasts
             acc = sum(forecasts.loc[d, (c, )].values.argmax(axis=1) == forecasts.loc[d, ('truth', )].values.argmax(axis=1)) / 100
 
+            # Bootsrapped CI for accuracy
             acc_CI_low, acc_CI_high = bootstrapped_CI_acc(forecasts.loc[d, (c, )].values.argmax(axis=1), forecasts.loc[d, ('truth', )].values.argmax(axis=1))
 
-
+            # Calculate RPS
             frc = np.cumsum(forecasts.loc[d, (c, )],axis=1)
             target = np.cumsum(forecasts.loc[d, ('truth', )],axis=1)
             rps = np.mean(np.mean(np.power((target - frc), 2), axis=1))
 
+            # For all dates not in the tuning sample, caclulate RPS per asset (for Table 8)
             if i >= back:
                 rps_assets.loc[:, c] += np.mean(np.power((target - frc), 2), axis=1)
 
+            # Caclulate Kullback–Leibler divergence from the uniform distribution [0.2, 0.2, 0.2, 0.2, 0.2]
             kl = np.sum(np.log((forecasts.loc[d, (c, )] / 0.2)) * forecasts.loc[d, (c, )], axis=1).mean()
 
+            # Caclulate Expected Calibration Error
             ece = calc_ece(forecasts.loc[d, (c, )].values, forecasts.loc[d, ('truth', )].values.argmax(axis=1))
 
+            # Calculate pAUC for False Positive Rates in the range 0 to 0.25
             pauc = calc_pauc(forecasts.loc[d, ('truth', )].values, forecasts.loc[d, (c, )].values, fpr=0.25)
+
+            # Bootsrapped CI for pAUC
             auc_CI_low, auc_CI_high = bootstrapped_CI_auc(forecasts.loc[d, ('truth', )].values, forecasts.loc[d, (c, )].values)
 
             information[c + '_RPS'].loc[d] = rps
@@ -205,7 +223,6 @@ def caclulate_metrics(file_name, back):
 
             acc_CI_low, acc_CI_high = bootstrapped_CI_acc(calibrated_forecasts.loc[d, (c, )].values.argmax(axis=1), calibrated_forecasts.loc[d, ('truth', )].values.argmax(axis=1))
 
-
             frc = np.cumsum(calibrated_forecasts.loc[d, (c, )],axis=1)
             target = np.cumsum(calibrated_forecasts.loc[d, ('truth', )],axis=1)
             rps = np.mean(np.mean(np.power((target - frc), 2), axis=1))
@@ -215,6 +232,7 @@ def caclulate_metrics(file_name, back):
             ece = calc_ece(calibrated_forecasts.loc[d, (c, )].values, calibrated_forecasts.loc[d, ('truth', )].values.argmax(axis=1))
 
             pauc = calc_pauc(calibrated_forecasts.loc[d, ('truth', )].values, calibrated_forecasts.loc[d, (c, )].values, fpr=0.25)
+
             auc_CI_low, auc_CI_high = bootstrapped_CI_auc(calibrated_forecasts.loc[d, ('truth', )].values, calibrated_forecasts.loc[d, (c, )].values)
 
             calibrated_information[c + '_RPS'].loc[d] = rps
@@ -233,17 +251,25 @@ def caclulate_metrics(file_name, back):
         calibrated_information.to_excel(writer, sheet_name='Infocalib')
         rps_assets.to_excel(writer, sheet_name='RPS_assets')
 
+#################### FUNCTIONS ####################
 
 if __name__ == '__main__':
 
-    # To run: python Calculate_metrics.py --file_name Results_m6.xlsx --tuning_sample 12
+    # To run: python Calculate_metrics.py --REPLICATE_PAPER 1
+    # OR
+    # To run: python Calculate_metrics.py --FILE_NAME 'Results_m6.xlsx' --TUNING_SAMPLE 12
 
     parser = argparse.ArgumentParser(description='Calculate metrics')
-    parser.add_argument('--file_name', nargs='+', type=str, help="The file that contains the forecasts")
-    parser.add_argument('--tuning_sample', nargs='?', type=int, const=0, default=12)
+    parser.add_argument('--FILE_NAME', nargs='?', type=str, help="The file that contains the forecasts")
+    parser.add_argument('--TUNING_SAMPLE', nargs='?', type=int, const=0, default=12, help="The size of the tuning sample. 0 if no tuning sample exists.")
+    parser.add_argument('--REPLICATE_PAPER', nargs='?', type=int, const=1, default=0)
     args = parser.parse_args()
 
-    caclulate_metrics(args.file_name, args.tuning_sample)
+    if args.REPLICATE_PAPER:
+        caclulate_metrics('/data/Results_M6.xlsx',  12)
+        caclulate_metrics('/data/Results_v2.xlsx', 36)
+    else:
+        caclulate_metrics(args.FILE_NAME[0], args.TUNING_SAMPLE)
 
 
     print('\nTask completed...')
